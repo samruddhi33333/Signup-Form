e693f396b0b9f662a4b613d6aa36f4cffd230345
Samruddhi Malvankar <165154110+samruddhi33333@users.noreply.github.com>
2025-01-16 01:15:14 +0530

    signup.js

--- signup.js
+++ signup.js
@@ -6,6 +6,7 @@ const SignupForm = () => {
         username: '',
         email: '',
         password: '',
+        confirmPassword: '',
     });
 
     const handleChange = (e) => {
@@ -15,8 +16,19 @@ const SignupForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        // Check if passwords match
+        if (formData.password !== formData.confirmPassword) {
+            alert('Passwords do not match!');
+            return;
+        }
+
         try {
-            const response = await axios.post('http://localhost:5000/signup', formData);
+            const response = await axios.post('http://localhost:5000/signup', {
+                username: formData.username,
+                email: formData.email,
+                password: formData.password,
+            });
             alert(response.data.message);
         } catch (error) {
             console.error(error);
@@ -26,9 +38,38 @@ const SignupForm = () => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
-            <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
-            <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+            <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+            />
+            <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+            />
+            <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+            />
+            <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                required
+            />
             <button type="submit">Signup</button>
         </form>
     );
