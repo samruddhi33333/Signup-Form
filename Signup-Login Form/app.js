6a59df98f1c46c4af7fca736977e5694b95d2bad
Samruddhi Malvankar <165154110+samruddhi33333@users.noreply.github.com>
2025-01-17 18:43:48 +0530

    Create app.js

---
+++ Signup-Login Form/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Signup from './signup';
+import Login from './login';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
